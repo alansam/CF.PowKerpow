1dd8bad48a6b975e8dc75ffb6ad07fc9a3ffb126
Alan Sampson <alansamps@gmail.com>
2021-02-04 17:32:02 -0800

    Initial Commit

---
+++ CF.PowKerpow/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  CF.PowKerpow
+//
+//  Created by Alan Sampson on 2/4/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
